775066005fa05b241e3cc3b3be63595d6ff667ee
nzapanarcisse <nzapanarcisse@gmail.com>
2025-04-06 09:43:41 +0400

    Update iam.tf

--- Livrables/EKS-TF/modules/eks/iam.tf
+++ Livrables/EKS-TF/modules/eks/iam.tf
@@ -34,16 +34,16 @@ module "eks_admins_iam_role" {
   ]
 }
 
-# module "user_iam_user" {
-#   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
-#   version = "5.3.1"
-#
-#   name                          = "student5_olivier_devops"
-#   create_iam_access_key         = false
-#   create_iam_user_login_profile = false
-#
-#   force_destroy = true
-# }
+ module "user_iam_user" {
+   source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+   version = "5.3.1"
+
+   name                          = "student5_olivier_devops"
+   create_iam_access_key         = false
+   create_iam_user_login_profile = false
+
+   force_destroy = true
+ }
 
 module "allow_assume_eks_admins_iam_policy" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-policy"
